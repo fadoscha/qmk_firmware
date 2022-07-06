85fb57a5b631357789ffdeceb8c3ab1398d10613
Fabian Schaffner <fabian.schaffner@dkbcodefactory.com>
2022-07-06 22:36:17 +0200

    updated keyboard layer

--- keyboards/moonlander/keymaps/fadoscha/keymap.c
+++ keyboards/moonlander/keymaps/fadoscha/keymap.c
@@ -12,7 +12,7 @@
 #define FA_SLSH LT(NEO, KC_SLSH)
 
 #define FA_LBRC LOPT_T(KC_LBRC)
-#define FA_RBRC ROPT_T(KC_RBRC)
+#define FA_RBRC LT(NUM, KC_RBRC)
 
 #define FA_DEC LGUI(KC_MINUS)
 #define FA_INC LGUI(KC_EQUAL)
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    FA_NOM,            FA_MAG,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    FA_DEC,            FA_INC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FA_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    MA_0,              MA_1,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, FA_QUOT,
-        FA_LBRC, FA_Z,    FA_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, FA_DOT,  FA_SLSH, KC_RBRC,
+        FA_LBRC, FA_Z,    FA_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, FA_DOT,  FA_SLSH, FA_RBRC,
         KC_GRV,  QK_BOOT, KC_LCTL, KC_LOPT, KC_LCMD,          KC_ESC,            QK_BOOT,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
                                             FA_TAB,  FA_SPC,  FA_QOP,            FA_PLT,  FA_ENT,  FA_ESC
     ),
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NUM] = LAYOUT_moonlander(
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_0,    KC_9,    KC_8,    KC_7,    KC_6,    _______,           _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,           _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
